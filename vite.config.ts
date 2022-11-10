9335ec5e717c59429b9646107dd6eae98c27f211
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-11-10 17:13:24 -0500

    Add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tierbuilder/',
   optimizeDeps: {
     esbuildOptions: {
       define: {
