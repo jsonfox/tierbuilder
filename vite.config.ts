ba5bb3895357034ae061bc2d1b5928c680397375
JtheFox <60980739+JtheFox@users.noreply.github.com>
2024-10-10 14:38:57 -0400

    Fix tainted canvas error

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   base: '/tierbuilder/',
   optimizeDeps: {
     esbuildOptions: {
       define: {
         global: 'globalThis'
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        })
-      ]
+      }
     }
   }
 });
